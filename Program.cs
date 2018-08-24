842f52fa0870a346f6d8977a10c7c27bf579a2f1
deymusis <vulpinari@yandex.ru>
2018-08-24 17:42:55 +0300

    Создал простой калькулятор
    Signed-off-by: deymusis <vulpinari@yandex.ru>

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Пишем калькулятор*/
+
+            Console.WriteLine("The method calculate sum, difference, multiplication and division of numbers.");
+
+            Console.WriteLine("Please enter the first number");
+            String a = Console.ReadLine();
+            Console.WriteLine("Please enter the second number");
+            String b = Console.ReadLine();
+
+            int num1 = Int32.Parse(a);
+            int num2 = Int32.Parse(b);
+
+
+            Console.WriteLine("Yours results:");
+            int result = num1 + num2;
+            Console.WriteLine(result);
+
+            result = num1 - num2;
+            Console.WriteLine(result);
+
+            result = num1 * num2;
+            Console.WriteLine(result);
+
+            double quotient = (double) (num1) / num2;
+            Console.WriteLine(quotient);
+
+            Console.ReadLine();
+        }
+    }
+}
